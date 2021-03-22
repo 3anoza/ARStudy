4869cb5857775316e787c2138fcc309b8216108e
Zolotuxin-Bogdan <b.zolotuxin@gmail.com>
2021-03-22 16:47:51 +0200

    Pushed all

---
+++ Assets/Scripts/DeviceInfoOnUI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+namespace Assets.Scripts
+{
+    public class DeviceInfoOnUI : MonoBehaviour
+    {
+        public ARCameraManager CameraManager;
+        void Start()
+        {
+            var settings = CameraManager.currentConfiguration;
+            var framerate = settings.Value.framerate;
+            var res = settings.Value.resolution;
+
+        }
+
+        void Update()
+        {
+            
+        }
+    }
+}
