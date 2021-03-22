e20a78b02437ff252d04d838e812ed15b846f0b3
Yeremeev <tonaros.kill@gmail.com>
2021-03-22 12:45:09 +0200

    added and implemented square display logic

---
+++ Assets/Scripts/GUI/SquareDisplay.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace Assets.Scripts.GUI
+{
+    public class SquareDisplay : MonoBehaviour
+    {
+        public TextMeshProUGUI Square;
+        public static SquareDisplay Instance;
+
+        private const string SQUARE_CAPTION = "Object square: ";
+        
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        private void OnEnable()
+        {
+            Square.text = SQUARE_CAPTION;
+        }
+
+        public void UpdateSquare(Vector3[] vertices)
+        {
+            if (vertices == null)
+                throw new ArgumentNullException(nameof(vertices));
+            if (vertices.Length < 3)
+                throw new ArgumentOutOfRangeException(nameof(vertices));
+            
+            var square = Geometric.GetSquare(vertices);
+            if (square <= 0) 
+                throw new Exception("Square calculating error!");
+            
+            Square.text = SQUARE_CAPTION + square;
+        }
+    }
+}
