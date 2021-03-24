cf70ad25f833f861cd29e21650eee22795a08d5b
Yeremeev <tonaros.kill@gmail.com>
2021-03-24 12:17:03 +0200

    implemented udp NetHelper

---
+++ Assets/Network/PackageType.cs
@@ -0,0 +1,10 @@
+﻿namespace Assets.Network
+{
+    public enum PackageType
+    {
+        Message = 0x01,
+        File = 0x03,
+        FileRequest = 0x04,
+        FileListRequest = 0x02
+    }
+}
