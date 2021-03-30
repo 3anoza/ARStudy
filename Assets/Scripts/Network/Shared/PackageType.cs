ced9bcba8cf1496a789866b77d70d7b8f6e951dc
Yeremeev <tonaros.kill@gmail.com>
2021-03-30 18:09:03 +0300

    implemented packages types

---
+++ Assets/Scripts/Network/Shared/PackageType.cs
@@ -0,0 +1,30 @@
+﻿namespace Assets.Scripts.Network.Shared
+{
+    public enum PackageType
+    {
+        /// <summary>
+        /// request to connect to the server and register on its network
+        /// </summary>
+        ConnectionRequest = 0x01,
+        /// <summary>
+        /// received / sent package is a simple message
+        /// </summary>
+        Message = 0x03,
+        /// <summary>
+        /// request to track changes in the available server network
+        /// </summary>
+        Subscribe = 0x02,
+        /// <summary>
+        /// received / sent packet is a bunch of file name and its internal information
+        /// </summary>
+        File = 0x04,
+        /// <summary>
+        /// request to the server to get a list of files from the server network
+        /// </summary>
+        FileListRequest = 0x06,
+        /// <summary>
+        /// request to the server to send a file
+        /// </summary>
+        FileRequest = 0x05,
+    }
+}
